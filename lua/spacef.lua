d6c328cc19e14c3f54f6742b43f765996d2dd39d
felipeazs <felipeazs@gmail.com>
2024-01-06 18:55:46 -0300

    lsp.type.property

--- lua/spacef.lua
+++ lua/spacef.lua
@@ -73,7 +73,6 @@ Group.new("ErrorMsg", colors.black, colors.red2, styles.bold)
 Group.new("Exception", colors.white2, nil, nil)
 
 --F
-Group.new("@field.lua", colors.silver:saturate(0.40), nil, nil)
 Group.new("Float", colors.orange2, nil, nil)
 Group.new("FloatBorder", colors.gray, nil, nil)
 Group.new("FoldColumn", colors.gray, nil, nil)
@@ -93,6 +92,7 @@ Group.new("Keyword", colors.red, nil, styles.italic)
 --L
 Group.new("Label", colors.red, nil, nil)
 Group.new("LineNr", colors.blue2, nil, nil)
+Group.new("@lsp.type.property.lua", colors.silver:saturate(-0.10), nil, nil)
 
 --M
 Group.new("Macro", colors.red, nil, styles.italic)
